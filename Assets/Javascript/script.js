26b5b1fd5565c6ba06a940a16564a277e8d7475d
erin-maxson <erin@cohatch.com>
2022-05-23 15:47:39 -0700

    more psuedo code on js

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -1,5 +1,9 @@
 //Step 1: Display start page - title and paragraph with start button
-//Step 2: Timer will start when the start button is clicked on the start page
+//Step 2: Timer will start when the start button is clicked on the start page, total timer is 75 seconds w/ 15 seconds for each question
 //Step 3: Display question with 4 clickable answers on question page, show question page only
 //Step 4: Start page is hidden, only displaying the question page
-//Step 5: After click, show the status of "Correct!" or "Wrong!"
+//Step 5: After click, show the status of "Correct!" or "Wrong!" based on answer
+//Step 5a: Wrong answers should decrease the timer by 15 seconds. If you answer correctly, there is no penalty on the clock.
+//Step 6: After all questions are answered, you will be presented with the high score page to add your initials alongside your score and hit submit button to save it in local storage.
+//Step 7: Timer should be stopped when all questions are answered.
+//Step 8: Time leftover should become your score at the end.
