5789aef320160d2fa47d9e6cb74cf678a5f0d2bc
erin-maxson <erin@cohatch.com>
2022-05-23 15:41:33 -0700

    adding pseudo code from class preview

---
+++ Assets/Javascript/script.js
@@ -0,0 +1,5 @@
+//Step 1: Display start page - title and paragraph with start button
+//Step 2: Timer will start when the start button is clicked on the start page
+//Step 3: Display question with 4 clickable answers on question page, show question page only
+//Step 4: Start page is hidden, only displaying the question page
+//Step 5: After click, show the status of "Correct!" or "Wrong!"
