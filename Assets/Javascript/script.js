2cf795d0c6ced9e3abd7964719e4ea2bd516ad10
erin-maxson <erin@cohatch.com>
2022-05-25 16:00:55 -0700

    adding functionality for answers for questions

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -14,20 +14,54 @@ var introEl = document.querySelector('#intro');
 var questionEl = document.query('#questions');
 var timerEl = document.querySelector('#timer');
 var inputEl = document.querySelector('#input-init');
+var titleEl = document.querySelector('#title');
+var answer1El = document.querySelector('#answer1');
+var answer2El = document.querySelector('#answer2');
+var answer3El = document.querySelector('#answer3');
+var answer4El = document.querySelector('#answer4');
+
 var startQuizBtn = document.querySelector('#start-btn');
 
 var timeRemains = 75
+var clockID = []
+
+var question = [ {
+    title: "new question 1",
+    answers: ["Answer1, Answer 2, Answer 3, Answer 4"],
+    solution: "Answer 2"
+}]
+
+var index=0
 
 function countDown() {
     timerEl.textContent=timeRemains
     timeRemains--
-    
-}
 
+}
 
 function startGame() {
+    questionEl.classList.remove("hide");
+    introEl.classList.add("hide");
+    clockID = setInterval(countDown,1000);
+    displayQuestions();
+}
+
+function displayQuestions() {
+    titleEl.textContent = question[index].answers[0]
+    answer1El.textContent = question[index].answers[1]
+    answer2El.textContent = question[index].answers[2]
+    answer3El.textContent = question[index].answers[3]
+}
+
+function nextQuestion() {
 
 }
+
+answer1El.addEventListener("click", nextQuestion)
+answer2El.addEventListener("click", nextQuestion)
+answer3El.addEventListener("click", nextQuestion)
+answer4El.addEventListener("click", nextQuestion)
+
 
 startQuizBtn.addEventListener("click", startGame)
 
